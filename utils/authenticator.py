c8f126a1250f3eada97b6d05340e7824f991a2e4
felipe.serna <damsog38@gmail.com>
2022-06-10 13:55:49 -0500

    Fixed authenticator object

--- utils/authenticator.py
+++ utils/authenticator.py
@@ -2,13 +2,25 @@ import requests
 import json
 
 class Authenticator:
-    def __init__(self, username, password, url)-> None:
+    def __init__(self, url, username=None, password=None)-> None:
         self.url = url
-        credentials = { 'username': username, 'password': password }
 
+        if(username is not None and password is not None):
+            credentials = { 'username': username, 'password': password }
+            authentication = requests.post(self.url, credentials)
+
+            self.token = json.loads(authentication.text).data.token
+            self.is_authenticated = True
+        else:
+            self.is_authenticated = False
+            self.token = None
+    
+    def authenticate(self, username, password):
+        credentials = { 'username': username, 'password': password }
         authentication = requests.post(self.url, credentials)
 
         self.token = json.loads(authentication.text).data.token
-    
+        self.is_authenticated = True
+
     def get_token(self):
         return self.token
