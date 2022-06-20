663cc8caee2e1018d3b687dfc42459868a3110b4
kaiser24 <damsog38@gmail.com>
2022-06-20 15:51:27 -0500

    Fixing some text printing

--- libs/printer.py
+++ libs/printer.py
@@ -14,8 +14,8 @@ def print_lists(lists):
 
 def print_objects(objects):
     table = Table(box=box.SIMPLE, show_header=True, header_style='bold #2070b2')
-    table.add_column("", justify="right", style="green", no_wrap=True)
-    table.add_column("Title", justify="right", style="cyan", no_wrap=True)
+    table.add_column("", justify="right", style="green")
+    table.add_column("Title", justify="left", style="cyan")
     table.add_column("Description", style="green")
     table.add_column("Attachments", style="blue")
     table.add_column("Filters", style="green")
